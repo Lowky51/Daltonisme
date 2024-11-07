983493b7438c83f52e135ae1ba39c44010ce4f24
Lowky51 <allanfleaux3@gmail.com>
2024-11-07 10:12:54 +0100

    ajout progress bar

---
+++ create_div.js
@@ -0,0 +1,8 @@
+// Sélectionne l'élément avec l'animation
+const progress = document.querySelector('.progress');
+
+// Ajout de l'écouteur pour l'événement 'animationend'
+progress.addEventListener('animationend', () => {
+  // Redirige vers la page souhaitée
+  window.location.href = 'index.html'; // Remplacez par l'URL de la page cible
+});
