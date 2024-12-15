e532d4fe93051b9069ef61ede8aa920ed0921b33
Lowky51 <allanfleaux3@gmail.com>
2024-12-15 19:10:04 +0100

    Add files via upload

--- img_script.js
+++ img_script.js
@@ -1,9 +1,13 @@
 const dropArea = document.getElementById('drop-area');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const canvasOriginal = document.getElementById('canvas-original');
+const canvas1 = document.getElementById('canvas1');
+const canvas2 = document.getElementById('canvas2');
+const canvas3 = document.getElementById('canvas3');
+const contextOriginal = canvasOriginal.getContext('2d');
+const context1 = canvas1.getContext('2d');
+const context2 = canvas2.getContext('2d');
+const context3 = canvas3.getContext('2d');
 const fileInput = document.getElementById('file-input');
-
-let selectedMatrix = null;
 
 dropArea.addEventListener('dragover', (event) => {
     event.preventDefault();
@@ -24,6 +28,10 @@ dropArea.addEventListener('drop', (event) => {
     }
 });
 
+dropArea.addEventListener('click', () => {
+    fileInput.click();
+});
+
 fileInput.addEventListener('change', (event) => {
     const files = event.target.files;
     if (files.length > 0) {
@@ -38,31 +46,35 @@ function handleFile(file) {
     reader.onload = (e) => {
         const img = new Image();
         img.onload = () => {
-            const newWidth = img.width * 0.3;  // 30% de la taille
-            const newHeight = img.height * 0.3; // 30% de la taille 
-            
-            canvas.width = newWidth;
-            canvas.height = newHeight;
+            const newWidth = img.width * 0.5;  // 30% de la taille
+            const newHeight = img.height * 0.5; // 30% de la taille
 
-            context.drawImage(img, 0, 0, newWidth, newHeight);
-            
-            if (selectedMatrix) {
-                applyColorTransformation(selectedMatrix);
-            } else {
-                alert("Sélectionner un type de daltonisme avant !");
-            }
+            canvasOriginal.width = newWidth;
+            canvasOriginal.height = newHeight;
+            canvas1.width = newWidth;
+            canvas1.height = newHeight;
+            canvas2.width = newWidth;
+            canvas2.height = newHeight;
+            canvas3.width = newWidth;
+            canvas3.height = newHeight;
+
+            contextOriginal.drawImage(img, 0, 0, newWidth, newHeight);
+            context1.drawImage(img, 0, 0, newWidth, newHeight);
+            context2.drawImage(img, 0, 0, newWidth, newHeight);
+            context3.drawImage(img, 0, 0, newWidth, newHeight);
+
+            applyColorTransformation(context1, matrices[1]);
+            applyColorTransformation(context2, matrices[2]);
+            applyColorTransformation(context3, matrices[3]);
         };
         img.src = e.target.result;
     };
 
     reader.readAsDataURL(file);
-
-    
-
 }
 
-function applyColorTransformation(matrix) {
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+function applyColorTransformation(context, matrix) {
+    const imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
     const data = imageData.data;
 
     for (let i = 0; i < data.length; i += 4) {
@@ -76,37 +88,26 @@ function applyColorTransformation(matrix) {
     }
 
     context.putImageData(imageData, 0, 0);
-  
-    canvas.style.display = 'block';
+    context.canvas.style.display = 'block';
 }
-
 
 const matrices = {
-    1: [	
-        0.203876,	0.990338,	-0.194214, 0, 0, 
-        0.112975,	0.794542,	0.092483, 0, 0, 
--       0.005222,	-0.041043,	1.046265, 0, 0,
-        0, 0, 0, 1, 0],
-
-
-    2: [	
-        0.392952,	0.823610,	-0.216562, 0, 0,
-        0.263559,	0.690210,	0.046232, 0, 0, 
-        -0.011910,	0.040281,	0.971630, 0, 0,
-        0, 0, 0, 1, 0],
-
+    1: [
+        0.203876, 0.990338, -0.194214, 0, 0,
+        0.112975, 0.794542, 0.092483, 0, 0,
+        -0.005222, -0.041043, 1.046265, 0, 0,
+        0, 0, 0, 1, 0
+    ],
+    2: [
+        0.392952, 0.823610, -0.216562, 0, 0,
+        0.263559, 0.690210, 0.046232, 0, 0,
+        -0.011910, 0.040281, 0.971630, 0, 0,
+        0, 0, 0, 1, 0
+    ],
     3: [
-        1.278864,	-0.125333,	-0.153531, 0, 0,
-        -0.084748,	0.957674,	0.127074, 0, 0, 
-        -0.000989,	0.601151,	0.399838, 0, 0,
-        0, 0, 0, 1, 0]
+        1.278864, -0.125333, -0.153531, 0, 0,
+        -0.084748, 0.957674, 0.127074, 0, 0,
+        -0.000989, 0.601151, 0.399838, 0, 0,
+        0, 0, 0, 1, 0
+    ]
 };
-
-function setMatrix(matrixNumber) {
-    selectedMatrix = matrices[matrixNumber];
-    alert('Type de daltonisme sélectionné, veuillez insérer votre image.');
-}
-
-console.log(FileReader())
-
-
